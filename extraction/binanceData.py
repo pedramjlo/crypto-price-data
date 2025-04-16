383bb7ea5c6a75cfd109986c180b48b467bb5848
pedramjlo <pedram_sif@tuta.io>
2025-04-16 22:36:04 +0330

    added exhcnage info function

--- extraction/binanceData.py
+++ extraction/binanceData.py
@@ -17,8 +17,9 @@ logging.basicConfig(
 
 
 class DataExtraction:
-    def __init__(self, binance_api_path, data_params=None):
-        self.binance_api_path = binance_api_path
+    def __init__(self, binance_test_endpoint, exchange_info_url, data_params=None):
+        self.binance_test_endpoint = binance_test_endpoint
+        self.exchange_info_url = exchange_info_url
         self.data_params = data_params
 
 
@@ -27,20 +28,37 @@ class DataExtraction:
 
     def test_connection(self):
         try:
-            response = requests.get(self.binance_api_path, params=self.data_params)
+            response = requests.get(self.binance_test_endpoint, params=self.data_params)
             response.raise_for_status()  
             logging.info('Successfully connected to Binance')
             return response.json()
         except Exception as e:
             logging.error(f'Failed to connect to Binance, {e}')
+
+
+
+    def exchange_information(self):
+        try:
+            response = requests.get(self.exchange_info_url, params=self.data_params)
+            response.raise_for_status()  
+            logging.info('Retrieved exchange inforamtion successfully')
+            return response.json()
+        except Exception as e:
+            logging.error(f'Failed to retrieve exhange information, {e}')
         
 
 
 if __name__ == "__main__":
 
-    binance_api_path = 'https://api.binance.com/api/v3/ping'
+    binance_test_endpoint = 'https://api.binance.com/api/v3/ping'
 
-    extractor = DataExtraction(binance_api_path)
+    exchange_info_url = 'https://api.binance.com/api/v3/exchangeInfo'
+
+    extractor = DataExtraction(binance_test_endpoint, exchange_info_url)
+
+
 
     print(extractor.test_connection())
+
+    print(extractor.exchange_information())
 

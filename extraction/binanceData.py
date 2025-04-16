799a379a4cf6f682dc639185c650b74a4f4f9045
pedramjlo <pedram_sif@tuta.io>
2025-04-16 22:59:51 +0330

    added price funtion

--- extraction/binanceData.py
+++ extraction/binanceData.py
@@ -17,9 +17,10 @@ logging.basicConfig(
 
 
 class DataExtraction:
-    def __init__(self, binance_test_endpoint, exchange_info_url, data_params=None):
+    def __init__(self, binance_test_endpoint, exchange_info_url, daily_price_url, data_params=None):
         self.binance_test_endpoint = binance_test_endpoint
         self.exchange_info_url = exchange_info_url
+        self.daily_price_url = daily_price_url
         self.data_params = data_params
 
 
@@ -45,6 +46,28 @@ class DataExtraction:
             return response.json()
         except Exception as e:
             logging.error(f'Failed to retrieve exhange information, {e}')
+
+
+
+    def daily_crypto_price(self, crypto):
+        params = {
+            'symbol': crypto
+        }
+        try:
+            response = requests.get(self.daily_price_url, params=params)
+            response.raise_for_status()
+            logging.info('Successfully connected to Binance')
+            data = response.json()
+            return {
+                "current_price": data["lastPrice"],
+                "price_change": data["priceChangePercent"],
+                "high_price": data["highPrice"],
+                "low_price": data["lowPrice"]
+            }
+        except Exception as e:
+            logging.error(f'Failed to retrieve BITCOIN price, {e}')
+        
+
         
 
 
@@ -54,11 +77,16 @@ if __name__ == "__main__":
 
     exchange_info_url = 'https://api.binance.com/api/v3/exchangeInfo'
 
-    extractor = DataExtraction(binance_test_endpoint, exchange_info_url)
+    daily_price_url = 'https://api.binance.com/api/v3/ticker/24hr'
+
+    extractor = DataExtraction(binance_test_endpoint, exchange_info_url, daily_price_url)
 
 
 
     print(extractor.test_connection())
 
-    print(extractor.exchange_information())
+    # print(extractor.exchange_information())
+
+
+    print(extractor.daily_crypto_price(crypto='BTCUSDT'))
 

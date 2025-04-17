4057f6f864f8f603d22775afaf3646d2490243f1
pedramjlo <pedram_sif@tuta.io>
2025-04-17 15:29:10 +0330

    added the eastern time function

--- extraction/binanceData.py
+++ extraction/binanceData.py
@@ -1,6 +1,7 @@
 import requests
 import logging
-
+from datetime import datetime
+import pytz
 
 
 logging.basicConfig(
@@ -12,16 +13,26 @@ logging.basicConfig(
 
 
 
-
-
-
-
 class DataExtraction:
-    def __init__(self, binance_test_endpoint, exchange_info_url, daily_price_url, data_params=None):
-        self.binance_test_endpoint = binance_test_endpoint
+
+    def __init__(self, binance_test_url, exchange_info_url, daily_price_url, data_params=None):
+        self.binance_test_url = binance_test_url
         self.exchange_info_url = exchange_info_url
         self.daily_price_url = daily_price_url
         self.data_params = data_params
+
+
+    def get_eastern_time(self):
+        """ 
+        - checking if currently the time in Eastern time is 4PM (the market's closing time)
+        """
+        try:
+            ny_tz = pytz.timezone('America/New_York')
+            current_time_ny = datetime.now(ny_tz)
+            logging.info('The Eastern time received')
+            return current_time_ny.time()
+        except Exception as e:
+            logging.error(f'Failed to get the Eastern time, {e}')
 
 
 
@@ -29,7 +40,7 @@ class DataExtraction:
 
     def test_connection(self):
         try:
-            response = requests.get(self.binance_test_endpoint, params=self.data_params)
+            response = requests.get(self.binance_test_url, params=self.data_params)
             response.raise_for_status()  
             logging.info('Successfully connected to Binance')
             return response.json()
@@ -50,36 +61,65 @@ class DataExtraction:
 
 
     def daily_crypto_price(self, crypto):
+        request_time = datetime.now()
+
         params = {
             'symbol': crypto
         }
         try:
             response = requests.get(self.daily_price_url, params=params)
             response.raise_for_status()
-            logging.info('Successfully connected to Binance')
+            logging.info("Successfully connected to Binance")
             data = response.json()
             return {
                 "current_price": data["lastPrice"],
                 "price_change": data["priceChangePercent"],
                 "high_price": data["highPrice"],
-                "low_price": data["lowPrice"]
+                "low_price": data["lowPrice"],
+                "date": f"{request_time}"
             }
         except Exception as e:
-            logging.error(f'Failed to retrieve BITCOIN price, {e}')
+            logging.error(f"Failed to retrieve the crypto price, {e}")
         
 
+    ######################################
+
+    def daily_crypto_price(self, crypto):
+        request_time = datetime.now()
+
+        params = {
+            'symbol': crypto
+        }
+        
+        if request_time == self.get_us_market_close_time:
+            try:
+                response = requests.get(self.daily_price_url, params=params)
+                response.raise_for_status()
+                logging.info('Successfully connected to Binance')
+                data = response.json()
+                return {
+                    "current_price": data["lastPrice"],
+                    "price_change": data["priceChangePercent"],
+                    "high_price": data["highPrice"],
+                    "low_price": data["lowPrice"],
+                    "date": f"{request_time}"
+                }
+            except Exception as e:
+                logging.error(f'Failed to retrieve BITCOIN price, {e}')
+        else:
+            logging.error("")
         
 
 
 if __name__ == "__main__":
 
-    binance_test_endpoint = 'https://api.binance.com/api/v3/ping'
+    binance_test_url = 'https://api.binance.com/api/v3/ping'
 
     exchange_info_url = 'https://api.binance.com/api/v3/exchangeInfo'
 
     daily_price_url = 'https://api.binance.com/api/v3/ticker/24hr'
 
-    extractor = DataExtraction(binance_test_endpoint, exchange_info_url, daily_price_url)
+    extractor = DataExtraction(binance_test_url, exchange_info_url, daily_price_url)
 
 
 
@@ -88,5 +128,6 @@ if __name__ == "__main__":
     # print(extractor.exchange_information())
 
 
-    print(extractor.daily_crypto_price(crypto='BTCUSDT'))
+    # print(extractor.daily_crypto_price(crypto='BNBETH'))
 
+    print(extractor.get_eastern_time())

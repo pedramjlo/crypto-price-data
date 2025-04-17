76f9064c94a19f4ec6e371265d6474b079568624
pedramjlo <pedram_sif@tuta.io>
2025-04-17 22:59:37 +0330

    created load_to_postgresql dbms

---
+++ store_data/createDB.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load variables from .env
+
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASS')
+dbname = os.getenv('DB_NAME')
+
+df = pd.read_csv('../saved_data/saved_data.csv')
+
+engine = create_engine(f"postgresql://{user}:{password}@localhost/{dbname}")
+df.to_sql('crypto_data', engine, if_exists='append', index=False)
+
+
--- transformation/dataConvert.py
+++ transformation/dataConvert.py
@@ -27,3 +27,5 @@ class DataConvert:
             return None
 
 
+
+
